029ec4ee4c18a64c48c46486043dc5977fbb4ad9
Shreyas Sahoo <shreyas0502@gmail.com>
2026-03-20 21:33:18 +0530

    Add pattern printing program in Pattern3.c
    This program prints a pattern of continuous numbers row-wise, with each row containing an increasing number of elements.

---
+++ Pattern3.c
@@ -0,0 +1,18 @@
+// Print pattern of continuous numbers row-wise
+
+#include <stdio.h>
+
+int main() 
+{
+    int i, j;
+
+    for (i=1; i<=5; i++) 
+    {
+        for (j=1; j<=i; j++) 
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
