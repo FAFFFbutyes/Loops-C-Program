1cb6fcc1e54d93db2af6069b4341a5efef197dc8
Shreyas Sahoo <shreyas0502@gmail.com>
2026-03-20 21:57:50 +0530

    Add program to find all divisors of a number

---
+++ AllDivisors.c
@@ -0,0 +1,20 @@
+// Finding all divisors of a number
+
+#include <stdio.h>
+int main() 
+{
+    int num, i;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Divisors of %d are: ", num);
+
+    for(i=1; i<=num; i++) 
+    {
+        if(num%i==0) 
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
