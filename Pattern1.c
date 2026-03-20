1adcb204c0a1c9c48021e26bfb7d052bcad0b426
Shreyas Sahoo <shreyas0502@gmail.com>
2026-03-20 21:31:17 +0530

    Add pattern printing program in Pattern1.c
    This program prints a pattern of numbers from 1 to 10 in a row-wise format, with each row containing an increasing number of elements.

---
+++ Pattern1.c
@@ -0,0 +1,20 @@
+// Print pattern of numbers row wise (1 to 10)
+
+#include <stdio.h>
+
+int main() 
+{
+    int i, j, num=1;
+
+    for (i=1; i<=4; i++) 
+    {
+        for (j=1; j<=i; j++) 
+        {
+            printf("%d ", num);
+            num++; 
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
