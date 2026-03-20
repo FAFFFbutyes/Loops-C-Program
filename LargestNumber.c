71db3c81867d2eb144aaf5fe9f6b1329db049095
Shreyas Sahoo <shreyas0502@gmail.com>
2026-03-20 21:30:40 +0530

    Add program to find largest of three numbers

---
+++ LargestNumber.c
@@ -0,0 +1,25 @@
+// Largest number out of 3 inputs taken from user
+
+#include <stdio.h>
+
+int main() 
+{
+    int n1, n2, n3;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    if (n1 >= n2 && n1 >= n3) 
+    {
+        printf("The largest number is: %d\n", n1);
+    }
+    else if (n2 >= n1 && n2 >= n3) 
+    {
+        printf("The largest number is: %d\n", n2);
+    }
+    else {
+        printf("The largest number is: %d\n", n3);
+    }
+
+    return 0;
+}
