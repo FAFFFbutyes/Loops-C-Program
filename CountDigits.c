18e45c15cd5948c63792c0e746b918f503b7d6bc
Shreyas Sahoo <shreyas0502@gmail.com>
2026-03-20 21:56:40 +0530

    Add CountDigits program to count digits in a number

---
+++ CountDigits.c
@@ -0,0 +1,30 @@
+// Count digits
+
+#include <stdio.h>
+int main() 
+{
+    int num, count=0;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    if (num==0) 
+    {
+        count = 1;
+    } else 
+    {
+        if (num<0) 
+        {
+            num = -num;
+        }
+
+        while (num != 0) 
+        {
+            num = num/10;
+            count++;
+        }
+    }
+
+    printf("Number of digits is: %d\n", count);
+    return 0;
+}
